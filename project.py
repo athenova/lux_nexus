be0e899b7a987218004bfa2eeab2ce946d5f7ea9
athenova <aleksey.sergeyev@yandex.com>
2025-03-02 22:25:21 +0300

    Начало проекта

---
+++ project.py
@@ -0,0 +1,19 @@
+from simple_blogger import SimpleBlogger
+
+class Project(SimpleBlogger):
+    def __init__(self, **kwargs):
+        super().__init__(            
+            review_chat_id=-1002374309134,
+            text_ai_token_name='OPENAI_API_KEY',
+            ai_text_model='chatgpt-4o-latest',
+            text_base_url='https://api.openai.com/v1',
+            **kwargs)
+
+    def _task_converter(self, item):
+        return { 
+                "category": f"{item['category']}",
+                "description": f"{item['description']}",
+                "topic_image": f"Нарисуй картинку, вдохновлённую темой '{item['category']}'",
+                "topic_prompt": f"Выбери рандомно актуальную проблему по теме '{item['category']}', опиши проблему, выбери рандомно метод решения, опиши метод решения, используй смайлики, используй менее {self.topic_word_limit} слов",
+            }
+    
